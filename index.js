d8ec545befbc36476c9fb1351b4b4dbdf3de40c7
Natalie Kimani <anitakimani07@gmail.com>
2026-04-07 12:11:42 +0300

    scopeLab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution in this file!
+
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a burger
+function addBurger() {
+  // Function-scoped variable
+  const newBurger = 'Flatburger';
+  
+  // Add to burgers array
+  burgers.push(newBurger);
+}
+
+// Block that always runs
+if (true) {
+  // Block-scoped variable
+  const anotherNewBurger = 'Maple Bacon Burger';
+  
+  // Add to burgers array
+  burgers.push(anotherNewBurger);
+}
+
+// Function to change featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
+
+console.log(burgers);
+
+addBurger();
+console.log(burgers);
+
+console.log(featuredDrink);
+
+changeFeaturedDrink();
+console.log(featuredDrink);
